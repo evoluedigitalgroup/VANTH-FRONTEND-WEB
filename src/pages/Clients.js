c53fc0f4105ab91710719784b37d8906f24c7647
Chirag Lathiya <clathiya.work@gmail.com>
2024-01-29 12:21:25 +0530

    Resolved the pagination issue in Clients

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import AfterAuth from "../HOC/AfterAuth";
 import { Button, Card, Table } from "react-bootstrap";
 import TableNavbar from "../components/TableNavbar";
@@ -6,9 +6,97 @@ import NewClientAdd from "../components/Document/NewClientAdd";
 import { getDocumentList } from "../helper/API/document";
 import { getAllDocumentsList, getContactList } from "../helper/API/contact";
 import { contactTableData } from "../recoil/Atoms";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import Loader from "../components/Loader";
 import ContactTable from "../components/Contact/ContactTable";
+import { contactPaginationData, toReloadContactData } from "../recoil/PaginationAtoms/Contact";
+import { PAGE_LIMIT } from "../config";
+
+
+const ClientData = ({
+  search,
+  tableRow,
+  refresh,
+  setRefresh,
+  setTableRow,
+  searchResult,
+  setSearchResult,
+  filterVal,
+  active,
+  setActive,
+}) => {
+  const tableData = useRecoilValue(
+    contactPaginationData(searchResult ? search : (search = ""))
+  );
+
+  const [reloadVal, reloadData] = useRecoilState(toReloadContactData);
+  const totalPage = Math.ceil((tableData?.totalFindData || 1) / PAGE_LIMIT);
+  const [table, setTable] = useRecoilState(contactTableData);
+
+
+  useEffect(() => {
+    reloadData(reloadVal + 1);
+    setSearchResult(false);
+  }, [refresh]);
+  useEffect(() => {
+    // setTable(tableData?.findData);
+    setTableRow(tableData?.findData);
+  }, [tableData]);
+
+  useEffect(() => {
+    handleToggle();
+  }, [filterVal])
+
+  const handleToggle = () => {
+    if (filterVal === "Pending") {
+      setActive({
+        pending: true,
+        approved: false,
+        all: false,
+      });
+
+      const newData = tableData?.findData.filter((obj) => {
+        if (obj.contactApprove === "pending") {
+          return obj;
+        }
+      });
+      setTableRow(newData);
+    } else if (filterVal === "Approved") {
+      setActive({
+        pending: false,
+        approved: true,
+        all: false,
+      });
+      const newData = tableData?.findData.filter((obj) => {
+        if (obj.contactApprove === "approved") {
+          return obj;
+        }
+      });
+      setTableRow(newData);
+    } else {
+      setActive({
+        pending: false,
+        approved: false,
+        all: true,
+      });
+      setTableRow(tableData?.findData);
+    }
+  };
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <ContactTable
+        tableRow={tableRow}
+        refresh={refresh}
+        setRefresh={setRefresh}
+        tableDataArray={tableData}
+        totalPage={totalPage}
+      />
+    </Suspense>
+  );
+
+};
+
 
 const Clients = () => {
   const [tableRow, setTableRow] = useState([]);
@@ -19,9 +107,16 @@ const Clients = () => {
     approved: false,
     all: true,
   });
+  const [filterVal, setFilterVal] = useState("All");
 
   const [search, setSearch] = useState();
+
+  const [searchResult, setSearchResult] = useState(false);
+
+  const [show, setShow] = useState(false);
+
   const [newTableRow, setNewtableRow] = useState([]);
+
   const [table, setTable] = useRecoilState(contactTableData);
 
   useEffect(() => {
@@ -44,56 +139,13 @@ const Clients = () => {
 
   const onEnter = (e) => {
     if (e.key === "Enter") {
-      setLoading(true);
-      const submitData = {
-        search,
-      };
-      getContactList(submitData).then((res) => {
-        if (res.success) {
-          setTableRow(res.data.findData);
-          setLoading(false);
-        } else {
-          setLoading(false);
-        }
-      });
-    }
-  };
-
-  const handleToggle = (status) => {
-    if (status === "Pending") {
-      setActive({
-        pending: true,
-        approved: false,
-        all: false,
-      });
-
-      const newData = table.filter((obj) => {
-        if (obj.status === "pending") {
-          return obj;
-        }
-      });
-      setTableRow(newData);
-    } else if (status === "Approved") {
-      setActive({
-        pending: false,
-        approved: true,
-        all: false,
-      });
-      const newData = table.filter((obj) => {
-        if (obj.status === "approved") {
-          return obj;
-        }
-      });
-      setTableRow(newData);
+      setSearchResult(true);
     } else {
-      setActive({
-        pending: false,
-        approved: false,
-        all: true,
-      });
-      setTableRow(table);
+      // setSearch("");
+      setSearchResult(false);
     }
   };
+
 
   const [showNovaClientButtonClick, setshowNovaClientButtonClick] =
     useState(false);
@@ -137,36 +189,40 @@ const Clients = () => {
               <Button
                 className={`fs-color mx-2 border-0 ${active.pending ? "activeBtnTable" : "inActiveBtnTable"
                   }`}
-                onClick={(e) => handleToggle("Pending")}
+                onClick={(e) => setFilterVal("Pending")}
               >
                 Pendentes
               </Button>
               <Button
                 className={`fs-color  mx-2 border-0 ${active.approved ? "activeBtnTable" : "inActiveBtnTable"
                   }`}
-                onClick={(e) => handleToggle("Approved")}
+                onClick={(e) => setFilterVal("Approved")}
               >
                 Respondidas
               </Button>
               <Button
                 className={`fs-color px-4 border-0 ${active.all ? "activeBtnTable" : "inActiveBtnTable"
                   }`}
-                onClick={(e) => handleToggle("All")}
+                onClick={(e) => setFilterVal("All")}
               >
                 Todos
               </Button>
             </div>
           </TableNavbar>
-
-          {loading ? (
-            <Loader />
-          ) : (
-            <ContactTable
+          <Suspense fallback={<Loader />}>
+            <ClientData
               tableRow={tableRow}
               refresh={refresh}
               setRefresh={setRefresh}
+              search={search}
+              setTableRow={setTableRow}
+              searchResult={searchResult}
+              setSearchResult={setSearchResult}
+              filterVal={filterVal}
+              active={active}
+              setActive={setActive}
             />
-          )}
+          </Suspense>
         </Card>
         {showNovaClientButtonClick && (
           <NewClientAdd
