8ff50129894a50cf08a25979199a35e4d2eaf02e
Chirag Lathiya <clathiya.work@gmail.com>
2024-02-02 18:34:53 +0530

    Resolved the logout issue

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { profileAtom } from '../recoil/Atoms';
+import { useSetRecoilState } from "recoil";
 
 const Logout = () => {
+  const setProfile = useSetRecoilState(profileAtom);
+
   const Logouts = () => {
+    setProfile("")
     let navigate = useNavigate();
     localStorage.clear();
     setTimeout(() => {
