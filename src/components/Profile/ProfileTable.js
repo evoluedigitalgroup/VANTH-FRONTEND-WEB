b4219847e89b35b1d1746ed7d5fe28ae9f0371ae
Lucas Antunes <lucasssantunes@hotmail.com>
2024-06-03 12:51:31 -0300

    Edditing users table

--- src/components/Profile/ProfileTable.js
+++ src/components/Profile/ProfileTable.js
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import Pagination from "../Pagination";
 import RecordFound from "../RecordFound";
+import { formatarCPF, formatarCNPJ, formatarHorario } from "../../library/contentformater/ContentFormater";
 
 const ProfileTable = ({ tableRow }) => {
   const [tableData, setTableData] = useState(tableRow);
@@ -20,43 +21,39 @@ const ProfileTable = ({ tableRow }) => {
   }, [currentPage]);
 
   return (
-    <>
-      <Table className="p-3 table-fit text-wrap tbl-color-text" responsive>
+    <div className="table-responsive">
+      <Table className="p-3 table-fit text-wrap tbl-color-text" responsive striped bordered hover>
         {currentTableData.length ? (
           <thead>
             <tr style={{ fontSize: "12px" }}>
-              <th className="tbl-head-color " width={"25%"}>
-                Nome{" "}
-              </th>
-              <th className="tbl-head-color ">CPF</th>
-              <th className="tbl-head-color ">CNPJ</th>
-              <th className="tbl-head-color ">Email </th>
-              {/* <th className='tbl-head-color '>Data</th> */}
-              <th className="tbl-head-color text-center">Hora </th>
-              <th className="tbl-head-color text-center">Status </th>
+              <th className="tbl-head-color" width="25%">Nome</th>
+              <th className="tbl-head-color">CPF</th>
+              <th className="tbl-head-color">CNPJ</th>
+              <th className="tbl-head-color">Email</th>
+              <th className="tbl-head-color text-center">Data de criação</th>
+              <th className="tbl-head-color text-center">Status</th>
             </tr>
           </thead>
         ) : (
           ""
         )}
         {currentTableData.length ? (
-          <tbody id="profileTable">
-            {currentTableData?.map((obj, i) => (
-              <tr style={{ fontSize: "14px" }}>
+          <tbody>
+            {currentTableData.map((obj, i) => (
+              <tr key={i} style={{ fontSize: "14px" }}>
                 <td className="fw-bold">{obj.name}</td>
-                <td>{obj.CPF}</td>
-                <td>{obj.CNPJ}</td>
+                <td>{formatarCPF(obj.CPF)}</td>
+                <td>{formatarCNPJ(obj.CNPJ)}</td>
                 <td>{obj.email}</td>
-                {/* <td>{obj.date}</td> */}
-                <td className="text-center">{obj.time}</td>
-                <td className="text-end">
+                <td className="text-center">{formatarHorario(obj.createdAt)}</td>
+                <td className="text-center">
                   <Button
                     variant="success"
                     style={{
                       width: "100px",
                       fontSize: "14px",
                     }}
-                    className="border-0 p-0"
+                    className="p-0"
                     size="md"
                   >
                     Ativo
@@ -68,16 +65,17 @@ const ProfileTable = ({ tableRow }) => {
         ) : (
           <RecordFound label="Nenhum Registro Encontrado" />
         )}
-        <Pagination
-          className="pagination-bar"
-          currentPage={currentPage}
-          totalCount={tableData.length}
-          pageSize={PageSize}
-          onPageChange={(page) => setCurrentPage(page)}
-        />
       </Table>
-    </>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={tableData.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+    </div>
   );
+  ;
 };
 
 export default ProfileTable;
--- src/library/contentformater/ContentFormater.js
+++ src/library/contentformater/ContentFormater.js
@@ -10,7 +10,25 @@ const formatarTelefone = (telefone) => {
 
 const formatarCNPJ = (cnpj) => {
   cnpj = cnpj.replace(/\D/g, '');
-  return cnpj.replace(/(\d{3})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
-}
+  return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+};
 
-export { formatarCNPJ, formatarTelefone, formatarCPF }
+const formatarHorario = (isoString) => {
+  const date = new Date(isoString);
+
+  const pad = (num) => num.toString().padStart(2, '0');
+
+  const dia = pad(date.getDate());
+  const mes = pad(date.getMonth() + 1); // Os meses são baseados em zero, então somamos 1
+  const ano = date.getFullYear();
+
+  const horas = pad(date.getHours());
+  const minutos = pad(date.getMinutes());
+  const segundos = pad(date.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
+};
+
+
+
+export { formatarCNPJ, formatarTelefone, formatarCPF, formatarHorario }
